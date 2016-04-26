ab31d8b72ed56b21441b90108da29df5361698a6
Stu Peters <stu_peters187@hotmail.com>
2016-04-26 12:41:53 -0400

    fixed repo

---
+++ tues/max.rb
@@ -0,0 +1,24 @@
+# Find the maximum 
+def maximum(arr)
+  num = arr[0]
+  arr.each do |x|
+    if num < x
+      num = x
+    end
+  end
+  num
+end
+
+# expect it to return 42 below
+result = maximum([2, 42, 22, 02])
+puts "max of 2, 42, 22, 02 is: #{result}"
+
+# expect it to return nil when empty array is passed in
+result = maximum([])
+puts "max on empty set is: #{result.inspect}"
+
+result = maximum([-23, 0, -3])
+puts "max of -23, 0, -3 is: #{result}"
+
+result = maximum([6])
+puts "max of just 6 is: #{result}"
