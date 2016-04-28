ba11dd6eaebd0ed2fb9eca259673e620123cabd5
Stu Peters <stu_peters187@hotmail.com>
2016-04-28 12:29:34 -0400

    Thurs morning

--- wed/statesandcities.rb
+++ wed/statesandcities.rb
@@ -6,12 +6,18 @@ require 'pry'
   CA: "California",
   NY: "New York",
   MI: "Michigan"
+
 }
 
 @states[:NV] = "Nevada"
 @states[:UT] = "Utah"
 
 @cities = {
+  OR: ["Portland", "Salem"],
+  FL: ["Tampa", "Orlando"],
+  CA: ["Fresno", "Napa"],
+  NY: ["New York City", "Buffalo"],
+  MI: ["Grand Rapids", "Detroit"],
   NV: ["Reno", "Vegas"],
   UT: ["Salt Lake City", "Binbrook"]
 }
@@ -21,7 +27,7 @@ require 'pry'
 def describe_state
   puts "Please enter state code"
   key = gets.chomp.to_sym
-  puts "#{key} is for #{@states.fetch(key)}."
+  puts "#{key} is for #{@states.fetch(key)}. It has 2 major cities "
 end
 
 @taxes = {
@@ -29,7 +35,9 @@ end
   FL: 8,
   CA: 7.5,
   NY: 8.5,
-  MI: 9
+  MI: 9,
+  NV: 10,
+  UT: 9.5
 }
 
 def calculate_tax
@@ -43,10 +51,10 @@ end
 
 def find_state_for_city
   puts "Please enter city name"
-  city = gets.chomp
-  @cities.each do |k, v|
-    if v.include? city
-      puts "The state code is #{k}"
+  input_city = gets.chomp
+  @cities.each do |state, city|
+    if city.include? input_city
+      puts "The state code is #{@states[state]}"
     end
     
   end
