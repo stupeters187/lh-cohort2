2fa9739c6673905902375800b3bd29e2d1857319
Stu Peters <stu_peters187@hotmail.com>
2016-04-29 13:14:00 -0400

    fri midday

---
+++ fri/mathgame.rb
@@ -0,0 +1,22 @@
+=begin 
+
+greet users to game
+
+set player name
+set lives to 3
+set points to 0
+
+generate turns
+
+generate 2 numbers
+store the value of the numbers in "answer" variable
+output question to player1
+
+take player 1 input for answer to question
+compare player 1 input answer to "answer" variable
+output correct or incorrect
+increment lives and points accordingly
+
+
+
+=end
---
+++ fri/player.rb
@@ -0,0 +1,19 @@
+class Player
+
+  attr_accessor :name, :points, :lives
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+    @points = 0
+  end
+
+  def increase_point
+    @points += 1
+  end
+
+  def decrease_lives
+    @lives -= 1
+  end
+
+end
