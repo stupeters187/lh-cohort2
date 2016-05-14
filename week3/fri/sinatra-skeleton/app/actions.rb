051d4b036c69eeb5bd6ac0f0721f5ae0e3be8977
vagrant <vagrant@lighthouse-labs>
2016-05-14 19:22:20 +0000

    msg board v1

--- week3/fri/sinatra-skeleton/app/actions.rb
+++ week3/fri/sinatra-skeleton/app/actions.rb
@@ -11,3 +11,21 @@ end
 get '/messages/new' do
   erb :'messages/new'
 end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+    )
+  if @message.save
+    redirect '/messages'
+  else
+    erb :'/messages/new'
+  end
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'/messages/show'
+end
